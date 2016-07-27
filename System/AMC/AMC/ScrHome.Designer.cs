938446f5b963d0dea145acfc43c671fb61cfcf4c
Nicum Wijesuriya <nicum.wijesuriya@ifsworld.com>
2016-07-27 16:59:58 +0530

    cvcvvxcvxcvcxvxc

--- System/AMC/AMC/ScrHome.Designer.cs
+++ System/AMC/AMC/ScrHome.Designer.cs
@@ -28,109 +28,129 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScrHome));
 			this.groupBox2 = new System.Windows.Forms.GroupBox();
 			this.btnCancelAppointment = new System.Windows.Forms.Button();
 			this.btnSetAppointment = new System.Windows.Forms.Button();
 			this.groupBox3 = new System.Windows.Forms.GroupBox();
-			this.btnUpdateSchedule = new System.Windows.Forms.Button();
 			this.btnAddSchedule = new System.Windows.Forms.Button();
 			this.btnRegister = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
 			this.groupBox2.SuspendLayout();
 			this.groupBox3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// groupBox2
 			// 
 			this.groupBox2.Controls.Add(this.btnCancelAppointment);
 			this.groupBox2.Controls.Add(this.btnSetAppointment);
-			this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.groupBox2.Location = new System.Drawing.Point(37, 44);
+			this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox2.Location = new System.Drawing.Point(33, 119);
 			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(279, 261);
+			this.groupBox2.Size = new System.Drawing.Size(279, 270);
 			this.groupBox2.TabIndex = 1;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Patient";
 			// 
 			// btnCancelAppointment
 			// 
-			this.btnCancelAppointment.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnCancelAppointment.Location = new System.Drawing.Point(44, 153);
+			this.btnCancelAppointment.BackColor = System.Drawing.SystemColors.Control;
+			this.btnCancelAppointment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCancelAppointment.Location = new System.Drawing.Point(42, 161);
 			this.btnCancelAppointment.Name = "btnCancelAppointment";
-			this.btnCancelAppointment.Size = new System.Drawing.Size(194, 30);
+			this.btnCancelAppointment.Size = new System.Drawing.Size(194, 66);
 			this.btnCancelAppointment.TabIndex = 1;
 			this.btnCancelAppointment.Text = "Cancel Appointment";
-			this.btnCancelAppointment.UseVisualStyleBackColor = true;
+			this.btnCancelAppointment.UseVisualStyleBackColor = false;
 			this.btnCancelAppointment.Click += new System.EventHandler(this.btnCancelAppointment_Click);
 			// 
 			// btnSetAppointment
 			// 
-			this.btnSetAppointment.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnSetAppointment.Location = new System.Drawing.Point(44, 85);
+			this.btnSetAppointment.BackColor = System.Drawing.SystemColors.Control;
+			this.btnSetAppointment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnSetAppointment.Location = new System.Drawing.Point(42, 57);
 			this.btnSetAppointment.Name = "btnSetAppointment";
-			this.btnSetAppointment.Size = new System.Drawing.Size(194, 30);
+			this.btnSetAppointment.Size = new System.Drawing.Size(194, 66);
 			this.btnSetAppointment.TabIndex = 0;
 			this.btnSetAppointment.Text = "Appointment";
-			this.btnSetAppointment.UseVisualStyleBackColor = true;
+			this.btnSetAppointment.UseVisualStyleBackColor = false;
 			this.btnSetAppointment.Click += new System.EventHandler(this.btnSetAppointment_Click);
 			// 
 			// groupBox3
 			// 
-			this.groupBox3.Controls.Add(this.btnUpdateSchedule);
 			this.groupBox3.Controls.Add(this.btnAddSchedule);
 			this.groupBox3.Controls.Add(this.btnRegister);
-			this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.groupBox3.Location = new System.Drawing.Point(358, 44);
+			this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.groupBox3.Location = new System.Drawing.Point(354, 119);
 			this.groupBox3.Name = "groupBox3";
-			this.groupBox3.Size = new System.Drawing.Size(279, 261);
+			this.groupBox3.Size = new System.Drawing.Size(279, 270);
 			this.groupBox3.TabIndex = 2;
 			this.groupBox3.TabStop = false;
 			this.groupBox3.Text = "Doctor";
 			// 
-			// btnUpdateSchedule
-			// 
-			this.btnUpdateSchedule.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnUpdateSchedule.Location = new System.Drawing.Point(44, 188);
-			this.btnUpdateSchedule.Name = "btnUpdateSchedule";
-			this.btnUpdateSchedule.Size = new System.Drawing.Size(194, 30);
-			this.btnUpdateSchedule.TabIndex = 2;
-			this.btnUpdateSchedule.Text = "Update Schedule";
-			this.btnUpdateSchedule.UseVisualStyleBackColor = true;
-			this.btnUpdateSchedule.Click += new System.EventHandler(this.btnUpdateSchedule_Click);
-			// 
 			// btnAddSchedule
 			// 
-			this.btnAddSchedule.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnAddSchedule.Location = new System.Drawing.Point(44, 118);
+			this.btnAddSchedule.BackColor = System.Drawing.SystemColors.Control;
+			this.btnAddSchedule.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAddSchedule.Location = new System.Drawing.Point(40, 161);
 			this.btnAddSchedule.Name = "btnAddSchedule";
-			this.btnAddSchedule.Size = new System.Drawing.Size(194, 30);
+			this.btnAddSchedule.Size = new System.Drawing.Size(194, 66);
 			this.btnAddSchedule.TabIndex = 1;
 			this.btnAddSchedule.Text = "Add Schedule";
-			this.btnAddSchedule.UseVisualStyleBackColor = true;
+			this.btnAddSchedule.UseVisualStyleBackColor = false;
 			this.btnAddSchedule.Click += new System.EventHandler(this.btnAddSchedule_Click);
 			// 
 			// btnRegister
 			// 
-			this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnRegister.Location = new System.Drawing.Point(44, 49);
+			this.btnRegister.BackColor = System.Drawing.SystemColors.Control;
+			this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnRegister.Location = new System.Drawing.Point(40, 57);
 			this.btnRegister.Name = "btnRegister";
-			this.btnRegister.Size = new System.Drawing.Size(194, 30);
+			this.btnRegister.Size = new System.Drawing.Size(194, 66);
 			this.btnRegister.TabIndex = 0;
 			this.btnRegister.Text = "Register";
-			this.btnRegister.UseVisualStyleBackColor = true;
+			this.btnRegister.UseVisualStyleBackColor = false;
 			this.btnRegister.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(33, 5);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(112, 111);
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(151, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(500, 46);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "ACME MEDICAL CENTER";
 			// 
 			// ScrHome
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(666, 340);
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(659, 412);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.groupBox3);
 			this.Controls.Add(this.groupBox2);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "ScrHome";
 			this.Text = "Home";
 			this.groupBox2.ResumeLayout(false);
 			this.groupBox3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -141,8 +161,9 @@
 		private System.Windows.Forms.Button btnRegister;
 		private System.Windows.Forms.Button btnCancelAppointment;
 		private System.Windows.Forms.Button btnSetAppointment;
-		private System.Windows.Forms.Button btnUpdateSchedule;
 		private System.Windows.Forms.Button btnAddSchedule;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
 
 
 	}
