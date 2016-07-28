5b94dc607aa15cc9db0bb9187be3a918e5b9d45c
Nicum Wijesuriya <nicum.wijesuriya@ifsworld.com>
2016-07-28 12:05:22 +0530

    dsa

--- Database/Procedures.sql
+++ Database/Procedures.sql
@@ -288,6 +288,20 @@ END //
 DELIMITER ;
 
 
+DELIMITER //
+create procedure AvailableDoctors()
+BEGIN
+	Select distinct(DID), concat(Title,FName,' ',LName) from AvailableDoctors;
+END //
+DELIMITER ;
+
+DELIMITER //
+create procedure AvailableDoctorsForSpecialization(vSID int)
+BEGIN
+	Select DID, concat(Title,'', FName,' ', LName) from AvailableDoctors where SID = vSID;
+END //
+DELIMITER ;
+
 #Patient
 DELIMITER //
 create procedure AddPatient (
@@ -620,5 +634,14 @@ BEGIN
 END // 
 DELIMITER ;
 
-
+create or replace view AvailableDoctors
+AS 
+Select D.DID,D.Title, D.FName, D.LName, S.SID, Sch.StartTime, Sch.Date 
+From Doctor as D JOIN Speciality as S JOIN Doc_Spec as DS JOIN Schedule as Sch
+ON D.DID = DS.DID AND DS.SID = S.SID AND Sch.DID = D.DID
+where D.DID IN (
+	select DID 
+    From Schedule 
+    where Status = 2
+);
 
